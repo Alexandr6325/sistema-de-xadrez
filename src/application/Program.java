8c8771a9c0977b27c3637a6fd38ac7da3e3e6795
Alexandre Henrique <xandefernandes2016@gmail.com>
2024-01-25 10:37:58 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-	
+
 		while (true) {
 			try {
 				UI.clearScreen();
@@ -25,29 +25,28 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
+
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
+
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-			}
+			} 
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			}
+			} 
 			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
 		}
 	}
-
 }
